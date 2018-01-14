0a76e9c761e0d7f09c637693ffd7280e040b8cb5
GabrielRaposo <gabriel.s.raposo@gmail.com>
2018-01-14 18:15:03 -0200

    Comparação de imagem básica
    * Apertar "espaço" compara os pixels entre o plano e a imagem original e retorna um valor em % no Console

---
+++ Assets/Scripts/StageManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour {
+    [SerializeField] private Renderer paintedImage;
+    [SerializeField] private SpriteRenderer originalSprite;
+
+    Texture2D paintedTex;
+    StageState stageState;
+
+    void Update () {
+        switch (stageState) {
+            case StageState.Playing:
+                if (Input.GetKeyDown("space")) {
+                    stageState = StageState.Analysis;
+
+                    Sprite _sprite = originalSprite.sprite;
+
+                    paintedTex = paintedImage.sharedMaterial.mainTexture as Texture2D;
+                    float similarityValue = ImageComparer.CheckSimilarity(paintedTex, _sprite.texture);
+                    Debug.Log("similarityValue: " + (similarityValue * 100).ToString("000") + "%");
+                }
+            break;
+
+            default:
+            break;
+        }
+
+	}
+
+    private enum StageState { Playing, Analysis, Results, Transition  }
+}
